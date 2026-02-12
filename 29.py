46f01e238f6cc1c10b7b932994f83c9067fbfa9a
ABHINAV TIWARI <tiwariabhinav318@gmail.com>
2026-02-12 22:52:25 +0530

    Add files via upload

---
+++ 29.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
+    result = cinema[
+        (cinema['id'] % 2 == 1) &
+        (cinema['description'] != 'boring')
+    ]
+    return result.sort_values(by='rating', ascending=False)
