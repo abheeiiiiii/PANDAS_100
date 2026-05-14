3fa51cce518c565256c8442727dbf8d440eb835c
ABHINAV TIWARI <tiwariabhinav318@gmail.com>
2026-05-14 21:41:26 +0530

    Add files via upload

---
+++ 73.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def count_salary_categories(accounts: pd.DataFrame) -> pd.DataFrame:
+    
+    low_salary = len(accounts[accounts["income"] < 20000])
+    
+    average_salary = len(
+        accounts[
+            (accounts["income"] >= 20000) &
+            (accounts["income"] <= 50000)
+        ]
+    )
+    
+    high_salary = len(accounts[accounts["income"] > 50000])
+    
+    return pd.DataFrame({
+        "category": ["Low Salary", "Average Salary", "High Salary"],
+        "accounts_count": [low_salary, average_salary, high_salary]
+    })
